94c966789b9e119377d97b0c7af10ea77a584b8b
LittleLW8847 <31029060+LittleLW8847@users.noreply.github.com>
2023-09-23 19:33:58 +0700

    i forgor to upload since the beginning sry

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [ioRef, setIoRef] = useState(""); //input output
+  
+  const inputSet = (tempVal) => {
+    setIoRef(ioRef + tempVal.target.value)
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p className='ioScreen' placeholder='placeholder'>{ioRef}</p>
+      <div className='buttonSet'>
+        <div className='firstRow'>
+          <button>C</button>
+          <button value='/' onClick={inputSet}>/</button>
+          <button value='*'onClick={inputSet}>X</button>
+          <button value='-'onClick={inputSet}>-</button>
+        </div>
+        <div className='secondRow'>
+          <button value="7" onClick={inputSet}>7</button>
+          <button value="8" onClick={inputSet}>8</button>
+          <button value="9" onClick={inputSet}>9</button>
+          <button value="+" onClick={inputSet}>+</button>
+        </div>
+        <div className='thirdRow'>
+          <button value="4" onClick={inputSet}>4</button>
+          <button value="5" onClick={inputSet}>5</button>
+          <button value="6" onClick={inputSet}>6</button>
+        </div>
+        <div className='forthrow'>
+          <button value="1" onClick={inputSet}>1</button>
+          <button value="2" onClick={inputSet}>2</button>
+          <button value="3" onClick={inputSet}>3</button>
+        </div>
+        <div className='equalButton'>
+          <button>=</button>
+        </div>
+      </div>
     </div>
   );
 }
